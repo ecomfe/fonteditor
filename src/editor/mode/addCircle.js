72f89f6ebadba16fbd4895e93aefa8f1e7f4d48c
kekee000 <kekee000@gmail.com>
2014-09-17 19:58:13 +0800

    添加圆模式

---
+++ src/editor/mode/addCircle.js
@@ -0,0 +1,18 @@
+/**
+ * @file addCircle.js
+ * @author mengke01
+ * @date 
+ * @description
+ * 添加圆模式
+ */
+
+
+define(
+    function(require) {
+
+        var mode = {
+        };
+
+        return mode;
+    }
+);
