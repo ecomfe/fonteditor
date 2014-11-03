f84a0183f381999b62b68de1b8f183d704e70ec5
kekee000 <kekee000@gmail.com>
2014-11-03 21:18:18 +0800

    add width class

---
+++ src/ttf/enum/weightClass.js
@@ -0,0 +1,24 @@
+/**
+ * @file weightClass.js
+ * @author mengke01
+ * @date 
+ * @description
+ * 字体粗细度量
+ */
+
+
+define(
+    function(require) {
+        return {
+            1: 'Ultra-light',
+            2: 'Extra-light',
+            3: 'Light',
+            4: 'Semi-light',
+            5: 'Medium (normal)',
+            6: 'Semi-bold',
+            7: 'Bold',
+            8: 'Extra-Bold',
+            9: 'Ultra-bold'
+        };
+    }
+);
---
+++ src/ttf/enum/widthClass.js
@@ -0,0 +1,24 @@
+/**
+ * @file weightClass.js
+ * @author mengke01
+ * @date 
+ * @description
+ * 字体宽度度量
+ */
+
+
+define(
+    function(require) {
+        return {
+            1: 'Ultra-condensed',
+            2: 'Extra-condensed',
+            3: 'Condensed',
+            4: 'Semi-condensed',
+            5: 'Medium (normal)',
+            6: 'Semi-expanded',
+            7: 'Expanded',
+            8: 'Extra-expanded',
+            9: 'Ultra-expanded'
+        };
+    }
+);
