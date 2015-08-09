66bad42ddeceb578d8e9cb761af73f4af59fd08c
kekee000 <kekee000@126com>
2015-08-09 18:58:57 +0800

    modify online font

--- src/fonteditor/data/online-font.js
+++ src/fonteditor/data/online-font.js
@@ -7,75 +7,80 @@ define(
     function (require) {
         return [
             {
-                name: 'fonteditor.ttf',
+                name: 'fonteditor',
                 url: './font/fonteditor.ttf',
-                from: 'fonteditor图标库'
+                from: 'fonteditor图标'
             },
             {
-                name: 'fontawesome-webfont.woff',
+                name: 'fontawesome图标',
                 url: 'http://www.bootcss.com/p/font-awesome/assets/font/fontawesome-webfont.woff',
-                from: 'fontawesome图标库'
+                from: 'fontawesome'
             },
             {
-                name: 'baiduHealth.woff',
+                name: '百度移动图标',
+                url: 'http://m.baidu.com/static/index/iconfont/iconfont_78d9aaf6.woff',
+                from: 'baidu'
+            },
+            {
+                name: '百度健康图标',
                 url: 'http://vs-static.baidu.com'
                     + '/m-health/new-composite/asset/common/css/font/baiduHealth.woff',
-                from: '百度健康图标'
+                from: 'baidu'
             },
             {
-                name: 'fonteditor.woff',
+                name: '百度健康医药馆图标',
                 url: 'http://vs-static.baidu.com'
                     + '/m-health/med-detail/201412301413/asset/common/css/fonts/fonteditor.woff',
-                from: '百度健康医药馆图标'
+                from: 'baidu'
             },
             {
-                name: 'i-edu.woff',
+                name: '百度教育图标',
                 url: 'http://vs-static.baidu.com/edu/m/asset/common/css/font/i-edu.woff',
-                from: '百度教育图标'
+                from: 'baidu'
             },
             {
-                name: '阿里妈妈图标库',
-                url: 'http://at.alicdn.com/t/font_1438789372_0128245.ttf',
+                name: '阿里妈妈图标',
+                url: 'http://mkwiser.sinaapp.com/font/font_1438789372_0128245.ttf',
                 from: 'iconfont'
             },
             {
-                name: '淘宝图标库',
-                url: 'http:////at.alicdn.com/t/font_1438789398_3645205.ttf',
+                name: '淘宝图标',
+                url: 'http://mkwiser.sinaapp.com/font/font_1438789398_3645205.ttf',
                 from: 'iconfont'
             },
             {
-                name: '天猫图标库',
-                url: 'http://at.alicdn.com/t/font_1438789427_2512155.ttf',
+                name: '天猫图标',
+                url: 'http://mkwiser.sinaapp.com/font/font_1438789427_2512155.ttf',
                 from: 'iconfont'
             },
             {
-                name: '1688 图标库',
-                url: 'http://at.alicdn.com/t/font_1438789809_9884617.ttf',
+                name: '1688 图标',
+                url: 'http://mkwiser.sinaapp.com/font/font_1438789809_9884617.ttf',
                 from: 'iconfont'
             },
             {
-                name: '手机淘宝图标库',
-                url: 'http://at.alicdn.com/t/font_1438789803_2537308.ttf',
+                name: '手机淘宝图标',
+                url: 'http://mkwiser.sinaapp.com/font/font_1438789803_2537308.ttf',
                 from: 'iconfont'
             },
             {
-                name: '阿里通信图标库',
-                url: 'http://at.alicdn.com/t/font_1438789794_5659146.ttf',
+                name: '阿里通信图标',
+                url: 'http://mkwiser.sinaapp.com/font/font_1438789794_5659146.ttf',
                 from: 'iconfont'
             },
             {
-                name: 'Alibaba国际站图标库',
-                url: 'http://at.alicdn.com/t/font_1438789716_42745.ttf',
+                name: 'Alibaba国际站图标',
+                url: 'http://mkwiser.sinaapp.com/font/font_1438789716_42745.ttf',
                 from: 'iconfont'
             },
             {
-                name: '支付宝钱包图标库',
-                url: 'http://at.alicdn.com/t/font_1438789709_7418613.ttf',
+                name: '支付宝钱包图标',
+                url: 'http://mkwiser.sinaapp.com/font/font_1438789709_7418613.ttf',
                 from: 'iconfont'
             },
             {
-                name: '商家产品图标库',
-                url: 'http://at.alicdn.com/t/font_1438789702_183668.ttf',
+                name: '商家产品图标',
+                url: 'http://mkwiser.sinaapp.com/font/font_1438789702_183668.ttf',
                 from: 'iconfont'
             }
         ];
