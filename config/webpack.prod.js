6b64ef6185f65f667491982fd672621fe360e183
kekee000 <kekee000@gmail.com>
2019-11-14 23:18:09 +0800

    update builder

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -15,7 +15,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, '../dist'),
-        filename: 'js/[name].js'
+        filename: 'js/[name]-[chunkhash:8].js'
     },
     devtool: false,
     devServer: {
